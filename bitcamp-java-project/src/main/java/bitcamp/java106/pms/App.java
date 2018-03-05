923c73aaa1393945c07ef90a4e4e07801f2ec106
donhee <231313do@gmail.com>
2018-03-05 20:13:43 +0900

    '0305'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -1,7 +1,33 @@
 package bitcamp.java106.pms;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner keyScan = new Scanner(System.in);
+
+        String a;
+        String b;
+        int i;
+        String start;
+        String end;
+
+        System.out.print("팀명? ");
+        a = keyScan.nextLine();
+        System.out.print("설명? ");
+        b = keyScan.nextLine();
+        System.out.print("최대인원? ");
+        i = keyScan.nextInt();
+        System.out.print("시작일? ");
+        start = keyScan.next();
+        System.out.print("종료일? ");
+        end = keyScan.next();
+
+        System.out.println("------------------");
+
+        System.out.println("팀명: "+ a);
+        System.out.println("설명: \n" + b );
+        System.out.println("최대인원: "+ i +"명");
+        System.out.println("일자: "+ start + " ~ " + end);
     }
 }
