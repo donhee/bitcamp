8e811076918e80d30bc7a9bf67b30265ac3d2d90
donhee <231313do@gmail.com>
2018-04-04 17:46:10 +0900

    파일 정보 조회 - java.io.File 클래스

--- bitcamp-java-basic/src/step16/Exam02.java
+++ bitcamp-java-basic/src/step16/Exam02.java
@@ -1,46 +1,54 @@
-// 파일 다루기 - java.io.File 클래스
+// 파일 정보 조회 - java.io.File 클래스
 package step16;
 
 import java.io.File;
 
 public class Exam02 {
+
     public static void main(String[] args) throws Exception {
         // File 클래스
         // => 파일이나 디렉토리 정보를 관리
-        // => 파일이나 디렉토리를 생성, 삭제, 변경 가능
+        // => 파일이나 디렉토리를 생성,삭제,변경
         
-        // 1) 현재 폴더 정보 조회
+        // 1) 현재 파일 정보 조회
         // "."  - 현재 폴더
         // ".." - 상위 폴더
-        File currentDir = new File("./src/step16");
-        System.out.printf("폴더명: %s\n", currentDir.getName());
-        System.out.printf("경로: %s\n", currentDir.getPath());
-        System.out.printf("절대경로: %s\n", currentDir.getAbsolutePath());
-        System.out.printf("계산된절대경로: %s\n", currentDir.getCanonicalPath());
-        System.out.printf("총크기: %d\n", currentDir.getTotalSpace());
-        System.out.printf("남은크기: %d\n", currentDir.getFreeSpace());
-        System.out.printf("가용크기: %d\n", currentDir.getUsableSpace());
-        System.out.printf("디렉토리여부: %b\n", currentDir.isDirectory());
-        System.out.printf("파일여부: %b\n", currentDir.isFile());
-        System.out.printf("감춤폴더여부: %b\n", currentDir.isHidden());
-        System.out.printf("존재여부: %b\n", currentDir.exists());
-        System.out.printf("실행가능여부: %b\n", currentDir.canExecute());
+        File file1 = new File("src/step16/Exam01.java");
+        System.out.printf("파일명: %s\n", file1.getName());
+        System.out.printf("파일크기: %d\n", file1.length());
+        System.out.printf("경로: %s\n", file1.getPath());
+        System.out.printf("절대경로: %s\n", file1.getAbsolutePath());
+        System.out.printf("계산된 절대경로: %s\n", file1.getCanonicalPath());
         
-        System.out.println("-------------------------------");
+        // 존재하지 않는 폴더인 경우 크기를 알아낼 수 없다.
+        System.out.printf("총크기: %d\n", file1.getTotalSpace());
+        System.out.printf("남은크기: %d\n", file1.getFreeSpace());
+        System.out.printf("가용크기: %d\n", file1.getUsableSpace());
         
-        // 2) 상위 폴더 정보 조회
-        File parentDir = new File("../bitcamp-java-project/src");
-        System.out.printf("폴더명: %s\n", parentDir.getName());
-        System.out.printf("경로: %s\n", parentDir.getPath());
-        System.out.printf("절대경로: %s\n", parentDir.getAbsolutePath());
-        System.out.printf("계산된절대경로: %s\n", parentDir.getCanonicalPath());
-        System.out.printf("총크기: %d\n", parentDir.getTotalSpace());
-        System.out.printf("남은크기: %d\n", parentDir.getFreeSpace());
-        System.out.printf("가용크기: %d\n", parentDir.getUsableSpace());
-        System.out.printf("디렉토리여부: %b\n", parentDir.isDirectory());
-        System.out.printf("감춤폴더여부: %b\n", parentDir.isHidden());
-        System.out.printf("존재여부: %b\n", parentDir.exists());
-        System.out.printf("실행가능여부: %b\n", parentDir.canExecute());
+        // 존재하지 않는 폴더인 경우 정보를 알아낼 수 없다. 모두 false
+        System.out.printf("디렉토리여부: %b\n", file1.isDirectory());
+        System.out.printf("파일여부: %b\n", file1.isFile());
+        System.out.printf("감춤폴더: %b\n", file1.isHidden());
+        System.out.printf("존재여부: %b\n", file1.exists());
+        System.out.printf("실행가능여부: %b\n", file1.canExecute());
         
+        System.out.println("----------------------------");
+        
+        // 2) 존재하지 않는 파일 정보 조회
+        File file2 = new File("src/step16/ok.java");
+        System.out.printf("폴더명: %s\n", file2.getName());
+        System.out.printf("파일크기: %d\n", file2.length());
+        System.out.printf("경로: %s\n", file2.getPath());
+        System.out.printf("절대경로: %s\n", file2.getAbsolutePath());
+        System.out.printf("계산된 절대경로: %s\n", file2.getCanonicalPath());
+        System.out.printf("총크기: %d\n", file2.getTotalSpace());
+        System.out.printf("남은크기: %d\n", file2.getFreeSpace());
+        System.out.printf("가용크기: %d\n", file2.getUsableSpace());
+        System.out.printf("디렉토리여부: %b\n", file2.isDirectory());
+        System.out.printf("파일여부: %b\n", file2.isFile());
+        System.out.printf("감춤폴더: %b\n", file2.isHidden());
+        System.out.printf("존재여부: %b\n", file2.exists());
+        System.out.printf("실행가능여부: %b\n", file2.canExecute());
     }
+
 }
