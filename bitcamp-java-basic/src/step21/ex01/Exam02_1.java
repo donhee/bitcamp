21de48583f9116085ae019d571789f65f98f6006
donhee <231313do@gmail.com>
2018-04-10 19:08:21 +0900

    예외 처리 문법을 적용하기 전 - 예외 발생 시 시스템 멈춤 문제

--- bitcamp-java-basic/src/step21/ex01/Exam02_1.java
+++ bitcamp-java-basic/src/step21/ex01/Exam02_1.java
@@ -1,4 +1,4 @@
-// 예외 처리 문법을 적용하기 전 - 예외 발생 시 시스템 멈춤 문제
+// 예외 처리 문법을 적용하기 전 - 예외 발생 시 시스템 멈춤 문제 
 package step21.ex01;
 
 import java.util.Scanner;
@@ -14,9 +14,9 @@ public class Exam02_1 {
             int v1 = keyScan.nextInt();
             int v2 = keyScan.nextInt();
             
-            // 일반적인 예외의 경우 리턴 값을 검사하여 처리하면 된다.
+            // 다음과 일반적인 예외의 경우 리턴 값을 검사하여 처리하면 된다.
             // 문제는 0으로 나누는 경우에서 처럼 계산할 수 없는 예외 상황이 발생한 경우,
-            // JVM은 실행을 종료하게 된다.
+            // JVM은 실행을 종료하게 된다. 
             // => 0으로 나눌 때처럼 예외가 발생하더라도 JVM을 멈추지 않고 
             //    계속 정상적으로 실행되게 만드는 문법이 "예외처리"이다.
             int result = Calculator2.compute(op, v1, v2);
@@ -29,3 +29,5 @@ public class Exam02_1 {
         keyScan.close();
     }
 }
+
+
