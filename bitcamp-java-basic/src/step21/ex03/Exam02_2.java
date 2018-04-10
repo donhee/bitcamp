9c5f9b398b79efdabcc77a34adf5426b4fa74b09
donhee <231313do@gmail.com>
2018-04-10 21:07:39 +0900

    .

--- bitcamp-java-basic/src/step21/ex03/Exam02_2.java
+++ bitcamp-java-basic/src/step21/ex03/Exam02_2.java
@@ -7,7 +7,7 @@ public class Exam02_2 {
     static void m() throws RuntimeException {
         throw new RuntimeException(); // OK!
     }
-    
+     
     // Exception의 서브 클래스임에도 불구하고
     // RuntimeException 클래스인 경우 메서드 선언부에 예외를 던진다고 선언하지 않아도 된다.
     // 왜? 스텔스 모드(비유) 를 지원하기 위해 만든 예외이다.
