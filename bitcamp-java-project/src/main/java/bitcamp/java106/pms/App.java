8256d8895b6aa0fca9f53ec2665ebd8e58f9d2e7
donhee <231313do@gmail.com>
2018-04-10 13:42:45 +0900

    ver 25

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -31,17 +31,26 @@ public class App {
         TeamDao teamDao = (TeamDao) iocContainer.getBean(TeamDao.class);
         TaskDao taskDao = (TaskDao) iocContainer.getBean(TaskDao.class);
         TeamMemberDao teammemberDao = (TeamMemberDao) iocContainer.getBean(TeamMemberDao.class);
-        try {
-            boardDao.save();
-            classroomDao.save();
-            memberDao.save();
-            teamDao.save();
-            taskDao.save();
-            teammemberDao.save();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("데이터 저장 중 오류발생!");
-        }
+        
+        // 각가의 데이터 저장에 대해 예외 처리를 분리해야 한다.
+        // 그래야만 예외가 발생하더라도 다른 데이터 저장은 정상적으로 수행할 것이다.
+        try { boardDao.save(); } 
+        catch (Exception e) { System.out.println("게시물 데이터 저장 중 오류 발생!"); }
+        
+        try { classroomDao.save(); } 
+        catch (Exception e) { System.out.println("수업 데이터 저장 중 오류 발생!"); }
+        
+        try { memberDao.save(); } 
+        catch (Exception e) { System.out.println("회원 데이터 저장 중 오류 발생!"); }
+        
+        try { taskDao.save(); } 
+        catch (Exception e) { System.out.println("작업 데이터 저장 중 오류 발생!"); }
+        
+        try { teamDao.save(); } 
+        catch (Exception e) { System.out.println("팀 데이터 저장 중 오류 발생!"); }
+        
+        try { teammemberDao.save(); } 
+        catch (Exception e) { System.out.println("팀 데이터 저장 중 오류 발생!"); }
     }
 
     static void onHelp() {
@@ -65,10 +74,6 @@ public class App {
         iocContainer = new ApplicationContext(
                 "bitcamp.java106.pms", defaultBeans);
         
-        // 테스트용 데이터를 준비한다. 
-        prepareMemberData();
-        prepareTeamData();
-        
         Console.keyScan = keyScan;
 
         while (true) {
@@ -87,98 +92,29 @@ public class App {
             } else if (menu.equals("help")) {
                 onHelp();
             } else {
-                int slashIndex = menu.lastIndexOf("/");
-                String controllerKey = (slashIndex < 0) ? 
-                        menu : menu.substring(0, slashIndex);
-                
-                Controller controller = (Controller) iocContainer.getBean(controllerKey);
-                
-                if (controller != null) {
-                    controller.service(menu, option);
-                } else {
-                    System.out.println("명령어가 올바르지 않습니다.");
+                try {
+                    int slashIndex = menu.lastIndexOf("/");
+                    String controllerKey = (slashIndex < 0) ? 
+                            menu : menu.substring(0, slashIndex);
+                    
+                    Controller controller = (Controller) iocContainer.getBean(controllerKey);
+                    
+                    if (controller != null) {
+                        controller.service(menu, option);
+                    } else {
+                        System.out.println("명령어가 올바르지 않습니다.");
+                    }
+                } catch (Exception e) {
+                    if (keyScan.hasNextLine()) { // 키보드 입력으로 남은 잔여 데이터가 있다면,
+                        keyScan.nextLine(); // 읽어서 버린다.
+                    }
+                    System.out.println("작업 실행 중에 오류가 발생하였습니다.");
+                    System.out.println("명령을 다시 실행해주세요.");
                 }
             }
 
             System.out.println(); 
         }
     }
-    static void prepareMemberData() {
-        MemberDao memberDao = (MemberDao) iocContainer.getBean(
-                "bitcamp.java106.pms.dao.MemberDao");
-        
-        Member member = new Member();
-        member.setId("aaa");
-        member.setEmail("aaa@test.com");
-        member.setPassword("1111");
-        
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("bbb");
-        member.setEmail("bbb@test.com");
-        member.setPassword("1111");
-        
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("ccc");
-        member.setEmail("ccc@test.com");
-        member.setPassword("1111");
-        
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("ddd");
-        member.setEmail("ddd@test.com");
-        member.setPassword("1111");
-        
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("eee");
-        member.setEmail("eee@test.com");
-        member.setPassword("1111");
-        
-        memberDao.insert(member);
-    }
-    
-    static void prepareTeamData() {
-        
-        TeamDao teamDao = (TeamDao) iocContainer.getBean(
-                "bitcamp.java106.pms.dao.TeamDao");
-        TeamMemberDao teamMemberDao = (TeamMemberDao) iocContainer.getBean(
-                "bitcamp.java106.pms.dao.TeamMemberDao");
-        
-        Team team = new Team();
-        team.setName("t1");
-        team.setMaxQty(5);
-        team.setStartDate(Date.valueOf("2018-1-1"));
-        team.setEndDate(Date.valueOf("2018-5-30"));
-        teamDao.insert(team);
-        teamMemberDao.addMember("t1", "aaa");
-        teamMemberDao.addMember("t1", "bbb");
-        teamMemberDao.addMember("t1", "ccc");
-        
-        team = new Team();
-        team.setName("t2");
-        team.setMaxQty(5);
-        team.setStartDate(Date.valueOf("2018-2-1"));
-        team.setEndDate(Date.valueOf("2018-6-30"));
-        teamDao.insert(team);
-        teamMemberDao.addMember("t2", "ccc");
-        teamMemberDao.addMember("t2", "ddd");
-        teamMemberDao.addMember("t2", "eee");
-        
-    }
 }
-
-//ver 17 - Task 관리 기능 추가
-// ver 15 - TeamDao와 MemberDao 객체 생성. 
-//          팀 멤버를 다루는 메뉴 추가.
-
-
-
-
-
-
+// ver 25 - 예외 처리 코드 추가
