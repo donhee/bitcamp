2de08053059497176ab48850211ddab28c388569
donhee <231313do@gmail.com>
2018-04-23 20:35:05 +0900

    // JDBC 프로그래밍 개요 - JDBC 드라이버 준비

--- bitcamp-java-basic/src/stetp25/ex01/Exam01_1.java
+++ bitcamp-java-basic/src/stetp25/ex01/Exam01_1.java
@@ -1,15 +1,16 @@
-// JDBC 프로그래밍 개요 - JDBC 드라이버 타입과 준비
+// JDBC 프로그래밍 개요 - JDBC 드라이버 준비
 package stetp25.ex01;
 
 import java.sql.DriverManager;
 
-// JDBC 드라이버 다운로드 및 설정
-// 1) 직접 설정하기
-// - java-basic/lib 폴더 생성
-// - c:\Program Files\MySQL\Connector J 8.0\mysql-connector-java-8.0.11.jar 복사
-// - java-basic/lib 폴더에 붙여넣기
-// - 컴파일과 실행할 때 .jar 파일을 사용할 수 있도록 CLASSPATH에 추가한다.
-//   project 컨텍스트 메뉴/
+//JDBC 드라이버 다운로드 및 설정
+//1) 직접 설정하기
+//- java-basic/lib 폴더 생성
+//- c:\Program Files (x86)\MySQL\Connector J x.x\mysql-connector-java-x.x.jar 복사
+//- java-basic/lib 폴더에 붙여넣기
+//- 컴파일과 실행할 때 .jar 파일을 사용할 수 있도록 CLASSPATH에 추가한다.
+//project 컨텍스트 메뉴/build path/configure build path.../Libraries/Add Jars...
+//
 public class Exam01_1 {
     public static void main(String[] args) throws Exception {
         // 1) JDBC 드라이버 로딩
