bd0501fadf6427934aaa0d8de35786c05c4b58df
donhee <231313do@gmail.com>
2018-04-26 10:42:57 +0900

    ver 32 - DB Connection Pool을 적용하여 DB 연결 객체를 효율적으로 관리하라.

---
+++ bitcamp-java-project/src32/main/java/bitcamp/java106/pms/controller/member/MemberDeleteController.java
@@ -0,0 +1,38 @@
+// Controller 규칙에 따라 메서드 작성
+package bitcamp.java106.pms.controller.member;
+
+import java.io.PrintWriter;
+
+import bitcamp.java106.pms.annotation.Component;
+import bitcamp.java106.pms.controller.Controller;
+import bitcamp.java106.pms.dao.MemberDao;
+import bitcamp.java106.pms.server.ServerRequest;
+import bitcamp.java106.pms.server.ServerResponse;
+
+@Component("/member/delete")
+public class MemberDeleteController implements Controller {
+    MemberDao memberDao;
+    
+    public MemberDeleteController(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public void service(ServerRequest request, ServerResponse response) {
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("id");
+        
+        try {
+            int count = memberDao.delete(id);
+    
+            if (count == 0) {
+                out.println("해당 아이디의 회원이 없습니다.");
+            } else {
+                out.println("회원 삭제하였습니다.");
+            }
+        } catch (Exception e) {
+            out.println("회원 삭제 실패!");
+            e.printStackTrace(out);
+        }
+    }
+    
+}
