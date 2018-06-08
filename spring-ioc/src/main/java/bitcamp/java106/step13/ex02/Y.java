2d4da3df44d86b138ccd7205125aefdd41aada23
donhee <231313do@gmail.com>
2018-06-08 16:05:01 +0900

    AOP

---
+++ spring-ioc/src/main/java/bitcamp/java106/step13/ex02/Y.java
@@ -0,0 +1,10 @@
+package bitcamp.java106.step13.ex02;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Y {
+    public void m2() {
+        System.out.println("Y.m2()");
+    }
+}
