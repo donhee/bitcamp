834412793389be15f98162cf8f30a3ecb6160ad2
donhee <231313do@gmail.com>
2018-06-12 11:31:27 +0900

    트랜잭션 정책 테스트

--- bitcamp-java-basic/src/step25/ex11/Exam01_insert.java
+++ bitcamp-java-basic/src/step25/ex11/Exam01_insert.java
@@ -1,4 +1,4 @@
-// mybatis + spring IoC - @Transactional 애노테이션으로 트랜잭션 적용 후
+// mybatis + spring IoC - 트랜잭션 정책 테스트
 package step25.ex11;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,30 +17,14 @@ public class Exam01_insert {
         // 
         BoardService boardService = iocContainer.getBean(BoardService.class);
 
-        Board b1 = new Board();
-        b1.setNo(181);
-        b1.setTitle("1111");
-        b1.setContent("xxxx");
+        Board b1 = new Board().setNo(191).setTitle("1111").setContent("x");
+        Board b2 = new Board().setNo(192).setTitle("2222").setContent("xx");
+        Board b3 = new Board().setNo(193).setTitle("3333").setContent("xxx");
+        Board b4 = new Board().setNo(191).setTitle("4444").setContent("xxxx");
+        Board b5 = new Board().setNo(195).setTitle("5555").setContent("xxxxx");
         
-        Board b2 = new Board();
-        b2.setNo(182);
-        b2.setTitle("2222");
-        b2.setContent("xxxx");
-        
-        Board b3 = new Board();
-        b3.setNo(181);
-        b3.setTitle("3333");
-        b3.setContent("xxxx");
-        
-        // test1() 메서드는 트랜잭션 매니저가 관리하기 때문에
-        // 실행 중에 오류가 발생하면 그 전에 실행했던 모든 결과를 rollback(취소)한다.
-        // 오류가 없어야만 commit 한다.
-        // boardService.test1(b1, b2, b3);
-        
-        // 그러나 test2() 메서드는 트랜잭션 매니저의 관리 대상이 아니기 때문에 
-        // 실행 중에 오류가 발생하면 그 전까지 실행된 결과를 그대로 유지한다.
-        boardService.test2(b1, b2, b3);
-        System.out.println("입력 성공!");
+        boardService.test1(b1, b2, b3, b4, b5);
+
     }
 }
 
