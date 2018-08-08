b279fbc00dba1f7d75493c48d70fdcf0376b6f5e
donhee <231313do@gmail.com>
2018-08-08 09:56:56 +0900

    .

--- test-project/src/main/java/bitcamp/java106/pms/service/impl/FacebookServiceImpl.java
+++ test-project/src/main/java/bitcamp/java106/pms/service/impl/FacebookServiceImpl.java
@@ -16,18 +16,8 @@ public class FacebookServiceImpl implements FacebookService {
         
         try {
             return restTemplate.getForObject(
-                    // 요청할 URL 
-                    // => URL에 들어갈 값이 있다면 중괄호{} 를 사용하여 지정하라.
-                    // => {변수명} 
                     "https://graph.facebook.com/v3.0/me?access_token={v1}&fields={v2}",
-                    
-                    // 서버에서 받은 값을 어떤 타입의 값으로 리턴할 지 지정하라.
                     type,
-                    
-                    // URL에 들어갈 값 
-                    // => 객체에 값을 담아서 전달하거나, 값을 순서대로 나열한다.
-                    // => 객체를 전달한다면 프로퍼티명으로 값을 찾아 삽입한다.
-                    // => 값을 나열한다면, 값이 나온 순서대로 삽입한다.
                     accessToken, "id,name,email");
             
         } catch (Exception e) {
