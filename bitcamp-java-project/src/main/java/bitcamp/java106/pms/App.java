c6ab1a56283a4e38192449171679bda5e9819aff
donhee <231313do@gmail.com>
2018-10-22 23:04:33 +0900

    test

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner keyScan = new Scanner(System.in);
-        
         String a;
         String b;
         int i;
