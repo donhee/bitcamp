78f91ad3bd50dcc2fe62e6d6ea6b6d947241d54f
donhee <231313do@gmail.com>
2018-10-31 13:48:54 +0900

    제네릭 적용 전 LinkedList

--- bitcamp-java-basic/src/step13/ex02/Exam01_1.java
+++ bitcamp-java-basic/src/step13/ex02/Exam01_1.java
@@ -7,11 +7,14 @@ public class Exam01_1 {
         list.add(new Member("홍길동", 10));
         list.add(new Member("임꺽정", 20));
         list.add(new Member("유관순", 30));
+        list.add(new Member("aaa", 30));
         
         // 값을 꺼내 사용하려면 항상 형변환해야 한다.
         for (int i = 0; i < list.size(); i++) {
             Member member = (Member) list.get(i);
-            System.out.printf("%s(%d)\n", member.name, member.age);
+           
+        //System.out.printf("%s(%d)\n", member.name, member.age);
+        System.out.println(String.format("%s,%d\n", member.name, member.age));
         }
     }
 }
