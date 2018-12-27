bbb48c4c719cb20e857bae4b545bd2bc40f08ce7
donhee <231313do@gmail.com>
2018-12-27 16:14:40 +0900

    .

---
+++ bitcamp-spring-ioc/src/main/java/org/springframework/step04/Exam01.java
@@ -0,0 +1,17 @@
+package org.springframework.step04;
+
+import org.springframework.BeanUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "org/springframework/step04/application-context-01.xml");
+        
+        System.out.println(iocContainer.getBean("c1"));
+        System.out.println(iocContainer.getBean("c2"));
+        System.out.println(iocContainer.getBean("c3"));
+        System.out.println(iocContainer.getBean("c4"));
+    }
+}
