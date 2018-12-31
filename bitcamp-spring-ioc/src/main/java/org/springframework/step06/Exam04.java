9fe7f671781e63132462918e819f30936fd96cf6
donhee <231313do@gmail.com>
2018-12-31 14:04:30 +0900

    팩토리 메서드를 통해 객체를 만들기 : 스프링 규칙에 따라 만들기

---
+++ bitcamp-spring-ioc/src/main/java/org/springframework/step06/Exam04.java
@@ -0,0 +1,15 @@
+package org.springframework.step06;
+
+import java.sql.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "org/springframework/step06/application-context-04.xml");
+    
+        System.out.println(iocContainer.getBean("c1"));
+    }
+}
