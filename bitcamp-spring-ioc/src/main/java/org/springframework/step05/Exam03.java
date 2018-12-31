f5005680ea8aabd175997202c46ebfcab11ed666
donhee <231313do@gmail.com>
2018-12-31 13:05:02 +0900

    Map 프로퍼티 값 설정하기

---
+++ bitcamp-spring-ioc/src/main/java/org/springframework/step05/Exam03.java
@@ -0,0 +1,13 @@
+package org.springframework.step05;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam03 {
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "org/springframework/step05/application-context-03.xml");
+        
+        System.out.println(iocContainer.getBean("c1"));
+    }
+}
