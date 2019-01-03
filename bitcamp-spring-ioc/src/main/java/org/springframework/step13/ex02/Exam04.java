dcb4a21377a63db6b9ea8b9a50263e7bac0534f2
donhee <231313do@gmail.com>
2019-01-03 16:35:21 +0900

    한 포인트 컷에 여러 개의 필터를 꼽을 때는 포인트 컷을 별도로 정의하는 것이 바람직하다.

---
+++ bitcamp-spring-ioc/src/main/java/org/springframework/step13/ex02/Exam04.java
@@ -0,0 +1,19 @@
+package org.springframework.step13.ex02;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "org/springframework/step13/ex02/application-context-04.xml");
+    
+        String[] names = iocContainer.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(iocContainer.getBean(name).getClass().getName());
+        }
+        System.out.println("-------------------");
+        
+        Caller caller = (Caller) iocContainer.getBean(Caller.class);
+        caller.test();
+    }
+}
