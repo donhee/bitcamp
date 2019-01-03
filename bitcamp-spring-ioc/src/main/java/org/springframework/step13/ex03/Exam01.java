101d300b1e32f61aa4e872c1fb27b498e0439ef6
donhee <231313do@gmail.com>
2019-01-03 16:53:39 +0900

    AOP 필터 적용 위치 설정

---
+++ bitcamp-spring-ioc/src/main/java/org/springframework/step13/ex03/Exam01.java
@@ -0,0 +1,23 @@
+package org.springframework.step13.ex03;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "org/springframework/step13/ex03/application-context-01.xml");
+    
+        String[] names = iocContainer.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(iocContainer.getBean(name).getClass().getName());
+        }
+        System.out.println("-------------------");
+        
+        try {
+            Caller caller = (Caller) iocContainer.getBean(Caller.class);
+            caller.test();
+        } catch (Exception e) {
+            System.out.println("오류발생");
+        }
+    }
+}
