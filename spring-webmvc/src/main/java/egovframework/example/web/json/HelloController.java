5cc390460d11a6b1630c48e69794e193c126dc55
donhee <231313do@gmail.com>
2019-01-04 15:31:41 +0900

    ContextLoaderListener 사용

--- spring-webmvc/src/main/java/egovframework/example/web/json/HelloController.java
+++ spring-webmvc/src/main/java/egovframework/example/web/json/HelloController.java
@@ -1,14 +1,13 @@
-package egovframework.example.web;
+package egovframework.example.web.json;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("web.HelloController")
+@Controller("web.json.HelloController")
 public class HelloController {
     
-    @RequestMapping("/hello")
+    @RequestMapping("/hello2")
     public String hello() {
-        return "/hello.jsp";
+        return "/hello2.jsp";
     }
-    
 }
