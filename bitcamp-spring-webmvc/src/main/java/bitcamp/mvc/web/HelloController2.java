6c793cbc714127d95ff57b91a3e555b92d0ae72d
donhee <231313do@gmail.com>
2019-01-04 13:19:47 +0900

    .

--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/HelloController2.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/HelloController2.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HelloController2 {
     
     @RequestMapping("/hello")
     public String hello() {
